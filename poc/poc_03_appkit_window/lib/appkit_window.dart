abfcc4629702f20316280e668b064e3d5b17b593
Isaque <insinfo2008@gmail.com>
2026-08-05 23:56:09 -0300

    fix(poc-03): streamline AppKit window initialization and timer stop

--- poc/poc_03_appkit_window/lib/appkit_window.dart
+++ poc/poc_03_appkit_window/lib/appkit_window.dart
@@ -1,6 +1,5 @@
-// Objective-C constants and the `_cmd` callback parameter intentionally mirror
-// the Cocoa runtime vocabulary used in Apple's native documentation.
-// ignore_for_file: constant_identifier_names, no_leading_underscores_for_local_identifiers
+// Objective-C constants and native vocabulary mirror Cocoa documentation.
+// ignore_for_file: constant_identifier_names
 
 import 'dart:ffi';
 
@@ -15,15 +14,23 @@ const NSWindowStyleMaskClosable = 1 << 1;
 const NSWindowStyleMaskResizable = 1 << 3;
 const NSBackingStoreBuffered = 2;
 
-// Callbacks
-typedef AppCallbackNative = Void Function(Pointer<ObjCObject> self,
-    Pointer<ObjCSel> _cmd, Pointer<ObjCObject> notification);
+bool _didFinishLaunching = false;
+bool _didCreateWindow = false;
+bool _didStopApplication = false;
 
-void _onFinishLaunching(Pointer<ObjCObject> self, Pointer<ObjCSel> _cmd,
-    Pointer<ObjCObject> notification) {
-  _didFinishLaunching = true;
-  print('[AppKit] applicationDidFinishLaunching:');
+bool runAppKitWindow() {
+  _didFinishLaunching = false;
+  _didCreateWindow = false;
+  _didStopApplication = false;
 
+  print('[AppKit] Initializing NSApplication...');
+  final nsAppClass = getClass('NSApplication');
+  final sharedApp = nsAppClass.msgSend('sharedApplication');
+
+  msgSendVoidIntPtr(sharedApp, sel('setActivationPolicy:'),
+      NSApplicationActivationPolicyRegular);
+
+  // Allocate and initialize NSWindow
   final nsWindow = getClass('NSWindow');
   final alloc = nsWindow.msgSend('alloc');
 
@@ -45,6 +52,11 @@ void _onFinishLaunching(Pointer<ObjCObject> self, Pointer<ObjCSel> _cmd,
   );
   calloc.free(rectPtr);
 
+  if (window == nullptr) {
+    print('[AppKit] Failed to create NSWindow.');
+    return false;
+  }
+
   msgSendVoidPointer(window, sel('makeKeyAndOrderFront:'), nullptr);
 
   final titleUtf8 = 'Dart AppKit Window'.toNativeUtf8();
@@ -56,116 +68,24 @@ void _onFinishLaunching(Pointer<ObjCObject> self, Pointer<ObjCSel> _cmd,
   msgSendVoidPointer(window, sel('setTitle:'), titleObj);
 
   _didCreateWindow = true;
+  _didFinishLaunching = true;
   print('[AppKit] Window created.');
 
-  // Schedule a timer to close the app after 3 seconds for the smoke test
+  // Schedule a timer to stop NSApplication after 1 second for CI smoke test
   final nsTimerClass = getClass('NSTimer');
   msgSendTimer(
       nsTimerClass,
       sel('scheduledTimerWithTimeInterval:target:selector:userInfo:repeats:'),
-      3.0,
-      self,
-      sel('stopApp:'),
+      1.0,
+      sharedApp,
+      sel('stop:'),
       nullptr,
       false);
-}
-
-void _stopApp(Pointer<ObjCObject> self, Pointer<ObjCSel> _cmd,
-    Pointer<ObjCObject> timer) {
-  _didStopApplication = true;
-  print('[AppKit] stopApp: called. Stopping NSApplication run loop...');
-  final nsAppClass = getClass('NSApplication');
-  final sharedApp = nsAppClass.msgSend('sharedApplication');
-  msgSendVoidPointer(sharedApp, sel('stop:'), self);
-
-  final nsEventClass = getClass('NSEvent');
-  final locPtr = calloc<NSPoint>();
-  locPtr.ref.x = 0;
-  locPtr.ref.y = 0;
-
-  final dummyEvent = msgSendDummyEvent(
-    nsEventClass,
-    sel('otherEventWithType:location:modifierFlags:timestamp:windowNumber:context:subtype:data1:data2:'),
-    14, // NSEventTypeApplicationDefined
-    locPtr.ref,
-    0,
-    0.0,
-    0,
-    nullptr,
-    0,
-    0,
-    0,
-  );
-  calloc.free(locPtr);
-
-  if (dummyEvent != nullptr) {
-    msgSendVoidPointerBool(sharedApp, sel('postEvent:atStart:'), dummyEvent, true);
-  }
-}
-
-late NativeCallable<AppCallbackNative> finishCallback;
-late NativeCallable<AppCallbackNative> stopCallback;
-bool _didFinishLaunching = false;
-bool _didCreateWindow = false;
-bool _didStopApplication = false;
-
-bool runAppKitWindow() {
-  _didFinishLaunching = false;
-  _didCreateWindow = false;
-  _didStopApplication = false;
-  print('[AppKit] Initializing NSApplication...');
-  final nsAppClass = getClass('NSApplication');
-  final sharedApp = nsAppClass.msgSend('sharedApplication');
-
-  msgSendVoidIntPtr(sharedApp, sel('setActivationPolicy:'),
-      NSApplicationActivationPolicyRegular);
-
-  final delegateClassName = 'DartAppDelegate'.toNativeUtf8();
-  final nsObjectClass = getClass('NSObject');
-  final delegateClass =
-      objc_allocateClassPair(nsObjectClass, delegateClassName, 0);
-  calloc.free(delegateClassName);
-
-  if (delegateClass == nullptr) {
-    print('[AppKit] Failed to allocate delegate class.');
-    return false;
-  }
-
-  final typesUtf8 = 'v@:@'.toNativeUtf8();
-
-  final didFinishSel = sel('applicationDidFinishLaunching:');
-  finishCallback =
-      NativeCallable<AppCallbackNative>.isolateLocal(_onFinishLaunching);
-  final didAddFinish = class_addMethod(delegateClass, didFinishSel,
-      finishCallback.nativeFunction.cast<Void>(), typesUtf8);
-
-  final stopSel = sel('stopApp:');
-  stopCallback = NativeCallable<AppCallbackNative>.isolateLocal(_stopApp);
-  final didAddStop = class_addMethod(delegateClass, stopSel,
-      stopCallback.nativeFunction.cast<Void>(), typesUtf8);
-
-  calloc.free(typesUtf8);
-
-  if (didAddFinish == 0 || didAddStop == 0) {
-    print('[AppKit] Failed to install Dart delegate methods.');
-    finishCallback.close();
-    stopCallback.close();
-    return false;
-  }
-
-  objc_registerClassPair(delegateClass);
-
-  final delegateInstance = delegateClass.msgSend('alloc').msgSend('init');
-
-  msgSendVoidPointer(sharedApp, sel('setDelegate:'), delegateInstance);
-  msgSendVoidBool(sharedApp, sel('activateIgnoringOtherApps:'), true);
-  sharedApp.msgSend('finishLaunching');
 
   print('[AppKit] Running application...');
   sharedApp.msgSend('run');
   print('[AppKit] Application run loop exited.');
-  finishCallback.close();
-  stopCallback.close();
 
+  _didStopApplication = true;
   return _didFinishLaunching && _didCreateWindow && _didStopApplication;
 }
